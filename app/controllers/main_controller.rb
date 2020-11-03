79db81accf0a292b9d91e61a509861dc1c36e6e5
Nicholas Marques <nnmarques97@gmail.com>
2020-11-03 17:49:30 -0300

    ✅ adicionado testes para o main controller

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,4 @@
+# Controller voltado para requisições gerais
 class MainController < ApplicationController
   def index
     render json: {api_version: "1.0"}
--- spec/requests/main_request_spec.rb
+++ spec/requests/main_request_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Mains", type: :request do
+RSpec.describe "MainController", type: :request do
 
-  describe "GET /index" do
-    it "returns http success" do
-      get "/main/index"
+  describe "GET Root" do
+    before do
+      get root_path
+    end
+    it "should return success status" do
       expect(response).to have_http_status(:success)
+    end
+
+    it 'should return the api version' do
+      expect(JSON.parse(response.body)['api_version']).to eq "1.0"
     end
   end
 
