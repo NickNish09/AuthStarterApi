9d00fd7cdfb35896a1c626e4e390f63ba5bb747d
Nicholas Marques <nnmarques97@gmail.com>
2020-11-03 18:46:42 -0300

    ✅ user controller request test

--- spec/requests/v1/user_request_spec.rb
+++ spec/requests/v1/user_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "V1::Users", type: :request do
+  describe "GET #current_user_data" do
+    context "when user is not authenticated" do
+      before do
+        get current_user_data_path
+      end
 
+      it 'should return an error message' do
+        expect(JSON.parse(response.body)['error']).to eq "você precisa estar autenticado ter acesso"
+      end
+    end
+  end
 end
