1ccec7887329a7d2a274854a204876de14217bd9
Nicholas Marques <nnmarques97@gmail.com>
2020-11-03 18:12:56 -0300

    ✨ Sign in e Sign up funcionando

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  scope :v1 do
+    mount_devise_token_auth_for 'User', at: 'auth'
+  end
   root to: 'main#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
