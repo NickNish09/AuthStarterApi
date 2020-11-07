cc7053507fb9bd5081689b6e1479941eaed0115d
Nicholas Marques <nnmarques97@gmail.com>
2020-11-07 11:00:02 -0300

    ✨ devise params adicionados

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name, :registration])
+  end
 end
